738103e0eae200769fd9937f0f4e4dc2d0f94ebd
piyushmittl <mr.piyushmittal@yahoo.com>
2018-01-25 11:33:33 +0530

    initail commit

---
+++ sender/src/main/java/com/piyush/mail/services/SendMailWithoutAttachmentTLSMain.java
@@ -0,0 +1,52 @@
+package com.piyush.mail.services;
+
+import java.io.IOException;
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Multipart;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+
+public class SendMailWithoutAttachmentTLSMain{
+
+	public static void main(String[] args) {
+
+		final String username = "pm31121988@gmail.com";
+		final String password = "***@";
+
+		Properties props = new Properties();
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.starttls.enable", "true");
+		props.put("mail.smtp.host", "smtp.gmail.com");
+		props.put("mail.smtp.port", "587");
+
+		Session session = Session.getInstance(props, new javax.mail.Authenticator() {
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication(username, password);
+			}
+		});
+
+		try {
+
+			Message message = new MimeMessage(session);
+			message.setFrom(new InternetAddress("pm31121988@gmail.com"));
+			message.setRecipients(Message.RecipientType.TO, InternetAddress.parse("mr.piyushmittal@rediffmail.com"));
+			message.setSubject("Testing Subject");
+			message.setText("Dear Mail Crawler," + "\n\n No spam to my email, please!");
+
+			Transport.send(message);
+
+			System.out.println("Done");
+
+		} catch (MessagingException e) {
+			throw new RuntimeException(e);
+		}
+	}
+}
